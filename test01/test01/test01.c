58fbe0b2d9194576a24b352fa19bba140f1466f5
hulklee1 <HulkLee@gmail.com>
2024-03-12 17:13:40 +0900

    1

--- test01/test01/test01.c
+++ test01/test01/test01.c
@@ -2,11 +2,15 @@
 #include <conio.h>
 int test01();		// 함수의 Prototype
 void test02(int a);
+void test03();
+void Dump(char* p, int len);	// 메모리 공간 출력용 범용함수
+void Copy(char* p1, char* p2);
 
 main()
 {
 	//test01();
-	test02(1);
+	//test02(1);
+	test03();
 }
 
 int test01()
@@ -42,20 +46,57 @@ int test01()
 		case 'Q': n = 0; break;
 		}*/
 		//int m = c - 0x30;  // ASCII --> num
-		printf("%c : %s\n", c, str[c - 0x30]);
+		printf("%c : %s\n", c, str[c - 0x30]);//str[n][m]
 	}
 }
 void test02(int a)  // 문자열과 문자배열
 {
 	char ca[] = "Hello"; // ca[0]:'H' ... ca[4]:'o' ca[5]:\0
-	for (int i = 0; i < 10; i++) // 0 ~ 5
+	for (int i = 0; i < 6; i++) // 0 ~ 5
 	{
-		printf("ca[%d] : %c (%02x)\n", i, ca[i], ca[i]);
+		printf("ca[%d] : %c (%02x) [%08x]\n", i, ca[i], ca[i], ca + i);
 	}
-	ca[2] -= 1;
-	ca[3] -= 1;
-	for (int i = 0; i < 10; i++) // 0 ~ 5
+	int ia[] = { 10,20,30,40,50 };
+	for (int i = 0; i < 6; i++) // 0 ~ 5
 	{
-		printf("ca[%d] : %c (%02x)\n", i, ca[i], ca[i]);
+		printf("ia[%d] : %d (%08x) [%08x]\n", i, ia[i], ia[i], ia + i);
+	}
+	int ia2[2][3] = {10,20,30,40,50,60};
+	for (int y = 0; y < 2; y++) // 0 ~ 2
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			printf("ia2[%d][%d] : %d [%08x]\n", y, x, ia2[y][x], ia2 + y);
+		}
+	}
+}
+void test03()
+{
+	char buf[100];  // 안전 메모리 공간 확보
+	char* pBuf;		// 안전 메모리 공간중의 출력 위치
+	unsigned int addr;		// 출력 위치 지정을 위한 입력변수(주소)
+	char kBuf[100];	// 출력 문자열 입력 공간
+	printf("안전공간의 주소는 %d[%08x] 입니다.\n", (unsigned int)buf, buf);
+	printf("입력을 시작할 주소를 입력하세요 : ");
+	scanf("%d", &addr);  // 안전공간 주소 참고 
+	pBuf = buf + addr;
+	printf("문자열을 입력하세요 : ");
+	scanf("%s", kBuf);
+	Copy(pBuf, kBuf);	// 문자열 복사
+	Dump(buf, 100);
+}
+
+void Copy(char* p1, char* p2)
+{
+	while (*p2) *p1++ = *p2++; *p1 = 0;
+}
+
+void Dump(char *p, int len)	// 메모리 공간 출력용 범용함수
+{
+	for (int i = 0; i < len; i++)	// 안전공간 메모리 덤프
+	{
+		if(i % 16 == 0) printf("\n%08x ", p);
+		if (i % 8 == 0) printf("  ");
+		printf("%02x ", (unsigned char)*p++);
 	}
 }
